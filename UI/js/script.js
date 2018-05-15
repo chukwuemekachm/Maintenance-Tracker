4433663d15353bd94fbc430188440d49ef12ffbb
CHIMA CHUKWUEMEKA EMMANUEL <chukwuemekachm@gmail.com>
2018-05-15 16:58:34 +0100

    Resolved contrast between text and foreground, resolved issues with modal

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,12 +1,23 @@
+/**
+ * Toggles Modals
+ * @param {string} id - The id of the Modal to be toggled 
+ * @param {object} me - The object calling the toggle method, {optional} 
+ */
 const toggleModal = (id, me) => {
     if (document.getElementById('modal').style.display === 'block') {
         document.getElementById('modal').style.display = 'none';
         document.getElementById(id).style.display = 'none';
         if (me) me.innerHTML = 'New Request';
     } else {
-        if (id === 'add-request') {
+        /** Device width */
+        const width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+        if (id === 'add-request' && width < 769) {
             document.getElementById('modal').style.top = '12em';
         } else {
+            document.getElementById('modal').style.top = '3.8em';
+        }
+        if (id === 'add-request' && width > 769) {
+            document.getElementById('modal').style.top = '3.8em';
             document.getElementById('modal').style.top = '3.8em';
         }
         document.getElementById('modal').style.display = 'block';

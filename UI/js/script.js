033436e2dd9f6aa909092298c5aff26bde372f92
CHIMA CHUKWUEMEKA EMMANUEL <chukwuemekachm@gmail.com>
2018-05-16 15:44:45 +0100

    [Feature #157564760] Completed admin view and manage requests page

--- UI/js/script.js
+++ UI/js/script.js
@@ -25,3 +25,37 @@ const toggleModal = (id, me) => {
         if (me) me.innerHTML = 'Cancel Request';
     }
 };
+
+/**
+ * Displays a custom message
+ * @param {string} message - The message to be displayed on the alert
+ * @param {number} id - The id which determines the backgroundcolor and message type
+ */
+const displayAlert = (message, id) => {
+    document.getElementById('display').style.display = 'block';
+    switch (id) {
+        case 1:
+            /** Success Message */
+            document.getElementById('display').style.backgroundColor = '#80ffc1';
+            document.getElementById('alert').innerHTML = message;
+            break;
+        case 2:
+            /** Information Message */
+            document.getElementById('display').style.backgroundColor = '#ffff80';
+            document.getElementById('alert').innerHTML = message;
+            break;
+        case 3:
+            /** Warning Message */
+            document.getElementById('display').style.backgroundColor = '#ff6666';
+            document.getElementById('alert').innerHTML = message;
+            break;
+        default:
+            /** Default Message */
+            document.getElementById('display').style.backgroundColor = '#4d94ff';
+            document.getElementById('alert').innerHTML = message;
+            break;
+    }
+    setTimeout(() => {
+        document.getElementById('display').style = 'none';
+    }, 4000);
+};

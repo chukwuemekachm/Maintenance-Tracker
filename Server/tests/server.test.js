eb0258f44ba4078609908b4035930b6f71bdc661
CHIMA CHUKWUEMEKA EMMANUEL <chukwuemekachm@gmail.com>
2018-05-17 23:25:05 +0100

    Added more descriptions to tests

--- Server/tests/server.test.js
+++ Server/tests/server.test.js
@@ -7,32 +7,32 @@ import { server } from '../../app';
 const should = chai.should();
 chai.use(chaiHttp);
 
-/**
- * tests
- * It should test baseurl and return 200 if it's successful
- * else it should return 404 if route is not supported
- *  */
 describe('GET /', () => {
-  it('should return 200 and message, "Welcome to Maintenance Tracker."', (done) => {
+  it('should return 200 and message, "Welcome to Maintenance Tracker"', (done) => {
     chai.request(server)
       .get('/')
       .end((req, res) => {
         res.should.have.status(200);
-        res.body.should.have.property('message');
+        res.should.be.a('object');
+        res.body.should.have.property('message').eql('Welcome to Maintenance Tracker');
         done();
       });
   });
 
-  it('should return 404 and message, "Route is not supported on the server." when an unavailable route is requested', (done) => {
+  it('should return 404 and message, "Route not supported on the server." when an unavailable route is requested', (done) => {
     chai.request(server).get('/api').end((req, res) => {
       res.should.have.status(404);
+      res.should.be.a('object');
+      res.body.should.have.property('message').eql('Route not supported on server.');
       done();
     });
   });
 
-  it('should return 404 and message, "Route is not supported on the server." when an unavailable route is requested', (done) => {
+  it('should return 404 and message, "Route not supported on the server." when an unavailable route is requested', (done) => {
     chai.request(server).get('/ap').end((req, res) => {
       res.should.have.status(404);
+      res.should.be.a('object');
+      res.body.should.have.property('message').eql('Route not supported on server.');
       done();
     });
   });

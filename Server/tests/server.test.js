eb2e7028c9bbddb24b166ac3a705b5f651d2fda4
CHIMA CHUKWUEMEKA EMMANUEL <chukwuemekachm@gmail.com>
2018-05-17 20:24:37 +0100

    [Chore #157658365] Setup testing framework

---
+++ Server/tests/server.test.js
@@ -0,0 +1,35 @@
+/** imports */
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import { server } from '../../app';
+
+/** instantiations */
+const should = chai.should();
+chai.use(chaiHttp);
+
+/** tests */
+describe('GET /', () => {
+  it('should return 200 and message, "Welcome to Maintenance Tracker."', (done) => {
+    chai.request(server)
+      .get('/')
+      .end((req, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property('message');
+        done();
+      });
+  });
+
+  it('should return 404 and message, "Route is not supported on the server." when an unavailable route is requested', (done) => {
+    chai.request(server).get('/api').end((req, res) => {
+      res.should.have.status(404);
+      done();
+    });
+  });
+
+  it('should return 404 and message, "Route is not supported on the server." when an unavailable route is requested', (done) => {
+    chai.request(server).get('/ap').end((req, res) => {
+      res.should.have.status(404);
+      done();
+    });
+  });
+});

84ce128f143f511ea1b85017c322dc71ae6e57a0
CHIMA CHUKWUEMEKA EMMANUEL <chukwuemekachm@gmail.com>
2018-05-17 18:58:00 +0100

    Resolved un necessary comments

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 /** imports */
 import express from 'express';
 import logger from 'morgan';
-import errorhandler from 'errorhandler';
-import bodyParser from 'body-parser';
 
 /** instantiations */
 const app = express();
 const port = process.env.PORT || 3000;
 
 /** middlewares */
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 app.use(logger('dev'));
-app.use(errorhandler());
 
 /** route handlers */
 app.get('/', (req, res) => res.status(200).json({ message: 'Welcome to Maintenance Tracker' }));
@@ -22,5 +17,4 @@ app.all('*', (req, res) => res.status(404).json({ message: 'Route not supported 
 /** bootups */
 app.listen(port, () => console.log(`Server listening on port ${port}...`));
 
-/** export */
 export const server = app;

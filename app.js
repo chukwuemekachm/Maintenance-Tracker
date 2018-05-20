ec30161808ad5289701656fe54435938f5a2b104
CHIMA CHUKWUEMEKA EMMANUEL <chukwuemekachm@gmail.com>
2018-05-20 03:31:58 +0100

    [Bug #157718641] Completes feedback on server

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-/** imports */
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 import RequestRoutes from './Server/routes/userRoutes';
 
-/** instantiations */
 const app = express();
 const port = process.env.PORT || 3000;
 
-/** middlewares */
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-/** route handlers */
-app.get('/', (req, res) => res.status(200).json({ message: 'Welcome to Maintenance Tracker' }));
+app.get('/', (req, res) => res.status(200).json({
+  status: 'success',
+  code: 200,
+  message: 'Welcome to Maintenance Tracker',
+}));
 
 app.use('/api/v1/users', RequestRoutes);
 
-app.all('*', (req, res) => res.status(404).json({ message: 'Route not supported on server.' }));
+app.all('*', (req, res) => res.status(404).json({
+  status: 'error',
+  code: 404,
+  message: 'Route not supported on server.',
+}));
 
-/** bootups */
-app.listen(port, () => console.log(`Server listening on port ${port}...`));
+app.listen(port);
 
 export const server = app;

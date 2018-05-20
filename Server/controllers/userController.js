28147c50e1b6546360dff4d01db154e60a34bf45
CHIMA CHUKWUEMEKA EMMANUEL <chukwuemekachm@gmail.com>
2018-05-20 01:09:31 +0100

    [Feature #157648006] Completed update request endpoint

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -74,6 +74,43 @@ class UserController {
       data: { request },
     });
   }
+
+  /**
+     * Updates an existing request when it exists
+     *
+     * @param {object} req - The request object received
+     * @param {object} res - The response object sent
+     *
+     * @returns {object}
+     */
+  updateRequest(req, res) {
+    if (!req.params.id || isNaN(req.params.id)) {
+      return res.status(400).json({
+        status: 'error',
+        code: 400,
+        message: 'Request id not valid',
+      });
+    }
+    const request = Request.getRequestById(parseInt(req.params.id, 10));
+    if (!request) {
+      return res.status(404).json({
+        status: 'fail',
+        code: 404,
+        message: 'Request not found',
+      });
+    }
+    request.title = req.body.title || request.title;
+    request.type = req.body.type || request.type;
+    request.description = req.body.description || request.description;
+    request.updatedAt = new Date().toLocaleTimeString();
+    Request.updateRequest(parseInt(req.params.id, 10), request);
+    return res.status(200).json({
+      status: 'success',
+      code: 200,
+      message: 'Request updated successfully',
+      data: { request },
+    });
+  }
 }
 
 export default UserController;

3f7bc12299d1ea6f944f09da8a94618aa1df0814
Chima Chukwuemeka <chukwuemekachm@gmail.com>
2019-02-15 11:27:49 +0100

    chore(AdminController): Integrate Data Access Layer on AdminController (#92)
    - Modify all CRUD operations on the AdminContoller to use the RequestRepository
    - Modify tests with new development
    - Modify EmailSender methods names
    
    [Completes #163355863]

--- Server/routes/adminRoutes.js
+++ Server/routes/adminRoutes.js
@@ -1,14 +1,35 @@
 import express from 'express';
-import AdminController from '../controllers/adminController';
+import AdminController from '../controllers/AdminContoller';
 import authenticateAdmin from '../middlewares/adminAuthentication';
 import { checkRequestId, filterRequest } from '../middlewares/validation';
-import { requestCheckPending, requestCheckApprove } from '../middlewares/checkRequest';
+import {
+  requestCheckPending,
+  requestCheckApprove,
+} from '../middlewares/checkRequest';
 
 const router = express.Router();
 
 router.get('/', authenticateAdmin, filterRequest, AdminController.getRequests);
-router.put('/:requestId/approve', checkRequestId, authenticateAdmin, requestCheckPending, AdminController.approveRequest);
-router.put('/:requestId/disapprove', checkRequestId, authenticateAdmin, requestCheckPending, AdminController.disapproveRequest);
-router.put('/:requestId/resolve', checkRequestId, authenticateAdmin, requestCheckApprove, AdminController.resolveRequest);
+router.put(
+  '/:requestId/approve',
+  checkRequestId,
+  authenticateAdmin,
+  requestCheckPending,
+  AdminController.approveRequest,
+);
+router.put(
+  '/:requestId/disapprove',
+  checkRequestId,
+  authenticateAdmin,
+  requestCheckPending,
+  AdminController.disapproveRequest,
+);
+router.put(
+  '/:requestId/resolve',
+  checkRequestId,
+  authenticateAdmin,
+  requestCheckApprove,
+  AdminController.resolveRequest,
+);
 
 export default router;
